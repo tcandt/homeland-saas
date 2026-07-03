3b731b35a5468058271e7ed63952a2882a8dbe25
tcandt <tinhyt2018@gmail.com>
2026-07-03 16:11:46 +0700

    fix(ci): add missing eslint config for api

---
+++ apps/api/.eslintrc.js
@@ -0,0 +1,25 @@
+// apps/api/.eslintrc.js
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-require-imports': 'off',
+    'no-case-declarations': 'off',
+    'no-empty': 'off'
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+};

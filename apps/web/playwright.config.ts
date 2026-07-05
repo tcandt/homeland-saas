4f8c76de2778010588ff30491b163370393eda77
tcandt <tinhyt2018@gmail.com>
2026-07-05 18:36:16 +0700

    fix(ci): use localhost in playwright config to support dual stack binding

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
     webServer: [
       {
               command: 'npm run start',
-              url: 'http://127.0.0.1:3000',
+              url: 'http://localhost:3000',
               reuseExistingServer: !process.env.CI,
               timeout: 120 * 1000,
               stdout: 'pipe',
@@ -59,7 +59,7 @@ export default defineConfig({
       },
       {
               command: 'npm run start:prod --workspace=api --prefix ../..',
-              url: 'http://127.0.0.1:3001',
+              url: 'http://localhost:3001',
               reuseExistingServer: !process.env.CI,
               timeout: 120 * 1000,
               stdout: 'pipe',
